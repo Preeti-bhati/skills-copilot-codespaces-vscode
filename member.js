f7f084c01b2b2ce6d5c27f66628766e67f1ca4dc
PREETI BHATI <142732574+Preeti-bhati@users.noreply.github.com>
2025-02-18 06:24:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsmember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs:'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        },
+    };
+}
